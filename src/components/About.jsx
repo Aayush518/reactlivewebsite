ad48df57a5d1e685ce6d580f264ce7c6d024d1b7
Aayush Adhikari <adhikariaayush37@gmail.com>
2023-07-07 22:39:56 +0545

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,11 +47,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+       I am a proficient graphics designer and full-stack web developer with expertise in utilizing Figma as a key tool in my design process. With a strong command over visual aesthetics and comprehensive web development skills, I am capable of delivering visually striking designs and creating highly functional web applications. By leveraging Figma's collaborative features, I can effectively collaborate with clients and stakeholders throughout the design process, ensuring a seamless and efficient workflow. I am committed to bringing your ideas to life and delivering exceptional results.
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
